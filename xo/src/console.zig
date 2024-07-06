b054c26adc192c31b06e32606b2eb9d619b5a954
Denys-Kalinovskyi <kdenis0610@gmail.com>
2024-07-06 23:14:59 +0300

    fix: minor adjustments to the console module

--- xo/src/console.zig
+++ xo/src/console.zig
@@ -1,7 +1,8 @@
 // A simple stdout wrapper
-const BUF_SIZE = 4; // Should be changed depending on use case (4096 is often used as a "gold" standard)
+const BUF_SIZE = 1; // Should be changed depending on use case (4096 is often used as a "gold" standard)
 
 const std = @import("std");
+const fmt = std.fmt.comptimePrint;
 
 pub const stdout = std.io.getStdOut().writer();
 pub var buffered_writer = std.io.bufferedWriter(stdout);
@@ -9,12 +10,12 @@ pub const stdout_buf = buffered_writer.writer();
 
 const stdin_file = std.io.getStdIn().reader();
 
-const esc = "\x1B";
-const clear_str = esc ++ "[2J";
+const esc = "\x1B["; // escape sequence
+const clear_str = esc ++ "2J";
 const reset_str = clear_str ++ getMoveStr(1, 1); // console is 1-indexed
 
 pub fn getMoveStr(x: u8, y: u8) []const u8 {
-    return esc ++ std.fmt.comptimePrint("[{d};{d}H", .{ x, y });
+    return esc ++ fmt("{d};{d}H", .{ x, y });
 }
 
 pub fn moveCursorTo(x: u8, y: u8) void {
